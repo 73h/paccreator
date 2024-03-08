ba2c66dfe65306b616fa027da28f9a73b2eca25a
heikoschmidt <heiko.schmidt@bkk-vbu.de>
2024-03-08 18:10:57 +0100

    i will never understand the thing with the file paths :facepalm:

--- src/pypacer/pypacer_test.py
+++ src/pypacer/pypacer_test.py
@@ -1,13 +1,16 @@
+import os
 import unittest
 
 from pypacer import PyPacer
 from pypacer.pypacerconfig import PyPacerConfig
 
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
 
 class TestPyPacer(unittest.TestCase):
 
     def setUp(self):
-        self.pac_file = open("/src/examples/pac_unittests.yaml", "r").read()
+        self.pac_file = open(os.path.join(__location__, "..", "examples", "pac_unittests.yaml"), "r").read()
 
     def test_load_config_from_yaml(self):
         p = PyPacer()
